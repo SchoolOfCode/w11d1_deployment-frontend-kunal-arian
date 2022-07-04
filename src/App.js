a3eabadd996fae58cf3c47da51bcd54dce5d3f83
ArianMoossavi <arianmoossavi@live.co.uk>
2022-07-04 16:24:18 +0100

    form added to src file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Form from "./Form.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>HelloWorld! it works!!! another attempt.</h1>
+      <h1>Form Submission App</h1>
+      <Form />
     </div>
   );
 }
